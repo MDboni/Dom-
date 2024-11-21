9e34873fcfc4b62c38dcec86e32282c33c4b15ef
boni <aminboni048@gmail.com>
2024-11-21 14:22:43 +0600

    3rd

---
+++ js/bundel.js
@@ -0,0 +1,79 @@
+
+// const nav =document.getElementById('nav');
+
+// const h2 = document.createElement('h2');
+// h2.innerText = 'amar sonar bangla';
+
+// const div = document.createElement('div')
+
+
+// const h3 = document.createElement('h3');
+// h3.innerText = 'oh amr sona pakih';
+
+// const ul = document.createElement('ul');
+// ul.innerHTML = `
+
+//         <li style="list-style-type = none">alul</li>
+//         <li>potol</li>
+
+// `
+
+
+// nav.appendChild(h2);
+// nav.appendChild(div);
+// div.appendChild(h3);
+// div.appendChild(ul);
+
+//  onclick ayer khela 
+
+
+
+// function clic(){
+//     const putput = document.getElementById('putput');
+//     putput.innerText =Date();
+    
+    
+// }
+
+// function hmm(){
+//     document.getElementById('putput').innerText= alert('valo khelaie shikcoh');
+// }
+
+// const kmmm = () => {
+//     document.body.style.backgroundColor ="red";
+// }
+
+// const nill = document.getElementById('nill');
+// nill.onclick =function ()  {
+//     document.body.style.backgroundColor= 'blue';
+// }
+
+// document.getElementById('addd').addEventListener('click', () => {
+//     document.body.style.backgroundColor = 'black';
+// })
+
+// const tuttut = document.getElementById('tuttut');
+// tuttut.onclick = arekjaygye ;
+
+// function arekjaygye(){
+//     document.body.style.backgroundColor = 'tomato';
+// }
+
+
+
+// <!-- addevenlistener details  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
